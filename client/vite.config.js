99b6f0fc06119724a09467184b00abd745928551
DilfaThayyil <dilfathayyil@gmail.com>
2025-04-01 19:39:45 +0530

    fix : resolving 404 NOT FOUND error when directly navigating to a route

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
+  base: "/", // Ensure correct base path
   server: {
-    historyApiFallback: true
+    historyApiFallback: true // Allow React Router navigation
   }
 });
